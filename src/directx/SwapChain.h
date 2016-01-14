e5b6c2eb4e5e3d68f2b2d427aa0e947c4f77bee0
berwyn <berwyn.codeweaver@gmail.com>
2016-01-14 13:14:19 -0700

    ComPtr to automate AddRef/Release on DXGI objects

--- src/directx/SwapChain.h
+++ src/directx/SwapChain.h
@@ -4,12 +4,14 @@
 #include <Windows.h>
 #include <d3d11.h>
 
+#include "Utils.h"
+
 struct SwapChain
 {
-	IDXGISwapChain* swapChain;
-	ID3D11Device* device;
-	D3D_FEATURE_LEVEL* featureLevel;
-	ID3D11DeviceContext* deviceContext;
+	ComPtr<IDXGISwapChain> swapChain;
+	ComPtr<ID3D11Device> device;
+	ComPtr<D3D_FEATURE_LEVEL> featureLevel;
+	ComPtr<ID3D11DeviceContext> deviceContext;
 };
 
 std::shared_ptr<SwapChain> CreateDXGISwapChain(HWND);
---
+++ src/directx/Utils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <atlbase.h>
+
+template<typename T>
+using ComPtr = ATL::CComPtr<T>;
