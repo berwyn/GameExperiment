a6682c38fc4f882424ab4079a868ba140fe6717d
berwyn <berwyn.codeweaver@gmail.com>
2016-01-19 10:46:51 -0700

    Finish ShaderLoader

--- src/directx/ShaderLoader.cpp
+++ src/directx/ShaderLoader.cpp
@@ -20,5 +20,79 @@ HRESULT ShaderLoader::LoadVertex(
 	{
 		HRESULT hr;
 		hr = device->CreateVertexShader(data, filesize, nullptr, shader);
+		if (hr != S_OK) { return hr; }
+		hr = device->CreateInputLayout(inputDesc, inputCount, data, filesize, layout);
+		if (hr != S_OK) { return hr; }
+		return S_OK;
+	});
+}
+
+HRESULT ShaderLoader::LoadHull(
+	ID3D11Device* device,
+	const wchar_t* path,
+	ID3D11HullShader** shader)
+{
+	if (!shader) { return E_POINTER; }
+	if (!path || !fs::exists(fs::path(path))) { return E_INVALIDARG; }
+
+	return WithMapped(path, [&](const BYTE* data, uint64_t filesize) -> HRESULT
+	{
+		return device->CreateHullShader(data, filesize, nullptr, shader);
+	});
+}
+
+HRESULT ShaderLoader::LoadDomain(
+	ID3D11Device* device,
+	const wchar_t* path,
+	ID3D11DomainShader** shader)
+{
+	if (!shader) { return E_POINTER; }
+	if (!path || !fs::exists(fs::path(path))) { return E_INVALIDARG; }
+
+	return WithMapped(path, [&](const BYTE* data, uint64_t filesize) -> HRESULT
+	{
+		return device->CreateDomainShader(data, filesize, nullptr, shader);
+	});
+}
+
+HRESULT ShaderLoader::LoadGeometry(
+	ID3D11Device* device,
+	const wchar_t* path,
+	ID3D11GeometryShader** shader)
+{
+	if (!shader) { return E_POINTER; }
+	if (!path || !fs::exists(fs::path(path))) { return E_INVALIDARG; }
+
+	return WithMapped(path, [&](const BYTE* data, uint64_t filesize) -> HRESULT
+	{
+		return device->CreateGeometryShader(data, filesize, nullptr, shader);
+	});
+}
+
+HRESULT ShaderLoader::LoadPixel(
+	ID3D11Device* device,
+	const wchar_t* path,
+	ID3D11PixelShader** shader)
+{
+	if (!shader) { return E_POINTER; }
+	if (!path || !fs::exists(fs::path(path))) { return E_INVALIDARG; }
+
+	return WithMapped(path, [&](const BYTE* data, uint64_t filesize) -> HRESULT
+	{
+		return device->CreatePixelShader(data, filesize, nullptr, shader);
+	});
+}
+
+HRESULT ShaderLoader::LoadCompute(
+	ID3D11Device* device,
+	const wchar_t* path,
+	ID3D11ComputeShader** shader)
+{
+	if (!shader) { return E_POINTER; }
+	if (!path || !fs::exists(fs::path(path))) { return E_INVALIDARG; }
+
+	return WithMapped(path, [&](const BYTE* data, uint64_t filesize) -> HRESULT
+	{
+		return device->CreateComputeShader(data, filesize, nullptr, shader);
 	});
 }
